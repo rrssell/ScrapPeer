9b2864247f5d734f7db63c724e78127feab8284c
rovie24 <130741329+rovie24@users.noreply.github.com>
2024-01-24 16:05:18 +0800

    Finished the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,4 +26,58 @@ const Footer = () => {
 
         </div>
         <div className={Styles.fm2}>
-        <div></div>
+        <div>
+            <h4>POPULAR LOCATIONS</h4>
+            <p><a href="#">Manila</a></p>
+           <p><a href="#">Navotas</a></p>
+           <p><a href="#">Malabon</a></p>
+           <p><a href="#">Caloocan</a></p>
+        </div>
+        <div>
+        <h4>TRENDING LOCATIONS</h4>
+            <p><a href="#">Manila</a></p>
+           <p><a href="#">Navotas</a></p>
+           <p><a href="#">Malabon</a></p>
+           <p><a href="#">Caloocan</a></p>
+        </div>
+        <div>
+        <h4>ABOUT US</h4>
+            <p><a href="#">About ScrapPeer Group</a></p>
+           <p><a href="#">Careers</a></p>
+           <p><a href="#">Contact Us</a></p>
+           <p><a href="#">ScrapPeer People</a></p>
+           
+        </div>
+        <div><h4>ScrapPeer</h4>
+            <p><a href="#">Help</a></p>
+           <p><a href="#">Sitemap</a></p>
+           <p><a href="#">Legal & Privacy information</a></p>
+           <p><a href="#">Blog</a></p></div>
+            <div className={Styles.fm2e}>
+               <div><h4>FOLLOW US</h4></div> 
+                <div className={Styles.social}>
+                    <p><a href="https://www.facebook.com/olxindia/" target="blank"><RiFacebookCircleLine /></a></p>
+                    <p><a href="https://www.instagram.com/olx_india/" target="blank"><RiInstagramLine /></a></p>
+                    <p><a href="https://twitter.com/OLX_India" target="blank"><TiSocialTwitterCircular /></a></p>
+                    <p><a href="https://www.youtube.com/user/OLXInTv" target="blank"><AiOutlineYoutube /></a></p>
+                </div>
+                    <div className={Styles.fimg}>
+                <img src="https://statics.OLX.in/external/base/img/playstore_2x.webp" alt="" width="90px" height="30px" marginRight="10px"/>
+                <img src="https://statics.OLX.in/external/base/img/appstore_2x.webp" alt="" width="90px" height="30px"/>
+                    </div>
+        </div>
+
+        </div>
+        <div className={Styles.fm3}>
+        <div>BSIT-NS-3A</div>
+        <div>WEB DEV PROJECT</div>
+
+        </div>
+
+        
+        </div>
+    
+  )
+}
+
+export default Footer
